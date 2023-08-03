267fd93a9a25849c55db8372294e0beb896a3f3d
dkdjiyeon <132960473+dkdjiyeon@users.noreply.github.com>
2023-08-03 16:13:24 +0900

    Update PlanVO.java

--- 230803/vo/PlanVO.java
+++ 230803/vo/PlanVO.java
@@ -17,4 +17,72 @@ public class PlanVO {
 		// TODO Auto-generated constructor stub
 	}
 
+	public PlanVO(MemberVO mvo, int planNo, Date departureDate, Date arrivalDate, String planJuso, String planTitle,
+			Date planDate) {
+		super();
+		this.mvo = mvo;
+		this.planNo = planNo;
+		this.departureDate = departureDate;
+		this.arrivalDate = arrivalDate;
+		this.planJuso = planJuso;
+		this.planTitle = planTitle;
+		this.planDate = planDate;
+	}
+
+	public MemberVO getMvo() {
+		return mvo;
+	}
+
+	public void setMvo(MemberVO mvo) {
+		this.mvo = mvo;
+	}
+
+	public int getPlanNo() {
+		return planNo;
+	}
+
+	public void setPlanNo(int planNo) {
+		this.planNo = planNo;
+	}
+
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public Date getArrivalDate() {
+		return arrivalDate;
+	}
+
+	public void setArrivalDate(Date arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+
+	public String getPlanJuso() {
+		return planJuso;
+	}
+
+	public void setPlanJuso(String planJuso) {
+		this.planJuso = planJuso;
+	}
+
+	public String getPlanTitle() {
+		return planTitle;
+	}
+
+	public void setPlanTitle(String planTitle) {
+		this.planTitle = planTitle;
+	}
+
+	public Date getPlanDate() {
+		return planDate;
+	}
+
+	public void setPlanDate(Date planDate) {
+		this.planDate = planDate;
+	}
+
 }
