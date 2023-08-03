a9aeb6232b2f37665d888cadba502f5abe1144fd
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-08-04 00:27:02 +0900

    Update CartVO.java

--- 230803/vo/CartVO.java
+++ 230803/vo/CartVO.java
@@ -6,17 +6,15 @@ public class CartVO {
 	private int cno;   					// 장바구니 아이디
 	private Date checkIn;       		// 체크인 날짜
 	private Date checkOut;      		// 체크아웃 날짜
-	private String refuContent;  	 	// 환불정책
 	private MemberVO mvo;				// 회원
 	private RoomVO rvo;					// 숙소
 	
 	
-	public CartVO(int cno, Date checkIn, Date checkOut, String refuContent, MemberVO mvo, RoomVO rvo) {
+	public CartVO(int cno, Date checkIn, Date checkOut, MemberVO mvo, RoomVO rvo) {
 		super();
 		this.cno = cno;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		this.refuContent = refuContent;
 		this.mvo = mvo;
 		this.rvo = rvo;
 	}
@@ -50,17 +48,6 @@ public class CartVO {
 	public void setCheckOut(Date checkOut) {
 		this.checkOut = checkOut;
 	}
-
-
-	public String getRefuContent() {
-		return refuContent;
-	}
-
-
-	public void setRefuContent(String refuContent) {
-		this.refuContent = refuContent;
-	}
-
 
 	public MemberVO getMvo() {
 		return mvo;
