e1a55431778249e721240e072c00dced81080ffb
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-08-04 15:12:31 +0900

    Update insert.sql

--- DB/insert.sql
+++ DB/insert.sql
@@ -1,1 +1,14 @@
 ~~~ 여기에다가 ~~~ 주석 달아서 부탁해요~~~
+
+-- 회원 INSERT
+INSERT INTO J_MEMBER
+VALUES('IAM','나다','1234','JEJUGAJA','ilovejeju@naver.com','010-3333-3333','남성',SYSDATE);
+
+-- 호스트 INSERT
+INSERT INTO HOST
+VALUES('host123','김호스트','123456','kimho','010-0000-0000','010-1111-1111','여성','hohoho@naver.com','123','사진'
+,SYSDATE,SYSDATE,SYSDATE);
+
+-- 숙소 INSERT
+INSERT INTO ROOM
+VALUES (SEQ_ROOM_rm_no.NEXTVAL, 'host123', '코지하우스', '애월~~~101호', 4, 150000,'이미지', '오션뷰', '체크인 시간 1시 체크아웃 2시입니다~~ 좋지요?', 0, 0);
