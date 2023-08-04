e0f57ccc8f71eb1a3c6e463521c6b51f8863fdf8
starstar <132961202+SustarJin@users.noreply.github.com>
2023-08-04 09:17:35 +0900

    Add files via upload

---
+++ 230803/vo/MateapplyVO.java
@@ -0,0 +1,71 @@
+package jejuOseyo.vo;
+
+public class MateapplyVO {
+	private int ano;  //목록 번호
+	private int mno;  //게시글 번호_MateVO 통일
+	private String mnick;  //신청자 닉네임_MemberVO 통일
+	private String mgender;	//신청자 성별__MemberVO 통일
+	private String mphone;	//신청자 전화번호__MemberVO 통일
+	private String mnow;  //신청내역 상태값
+	
+	public MateapplyVO(int ano, int mno, String mnick, String mgender, String mphone, String mnow) {
+		super();
+		this.ano = ano;
+		this.mno = mno;
+		this.mnick = mnick;
+		this.mgender = mgender;
+		this.mphone = mphone;
+		this.mnow = mnow;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public int getMno() {
+		return mno;
+	}
+
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+
+	public String getMnick() {
+		return mnick;
+	}
+
+	public void setMnick(String mnick) {
+		this.mnick = mnick;
+	}
+
+	public String getMgender() {
+		return mgender;
+	}
+
+	public void setMgender(String mgender) {
+		this.mgender = mgender;
+	}
+
+	public String getMphone() {
+		return mphone;
+	}
+
+	public void setMphone(String mphone) {
+		this.mphone = mphone;
+	}
+
+	public String getMnow() {
+		return mnow;
+	}
+
+	public void setMnow(String mnow) {
+		this.mnow = mnow;
+	}
+	
+	
+	
+}
